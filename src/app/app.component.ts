a962246f1b92e71c5d105215481f6de70a7e2866
Camille JOLLIET <cjolliet@laprovence.com>
2025-07-12 10:37:32 +0200

    ngx-translate install, fr.json, template, ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,9 @@ import { HeaderComponent } from "./shared/components/header/header.component";
   imports: [RouterOutlet, HeaderComponent]
 })
 export class AppComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('fr');
+    this.translate.use('fr');
+  }
   title = 'petricator';
 }
