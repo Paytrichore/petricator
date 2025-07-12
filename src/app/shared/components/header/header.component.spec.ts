285c50e6dd7e9e317badb17c4f95ca695e2dfba5
Camille JOLLIET <cjolliet@laprovence.com>
2025-07-12 10:55:54 +0200

    unit tests

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { TranslateService } from '@ngx-translate/core';
+import { translateServiceMock } from '../../../tests/mocks/translate.service.mock';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +9,10 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
+      imports: [HeaderComponent],
+      providers: [
+        { provide: TranslateService, useValue: translateServiceMock }
+      ]
     })
     .compileComponents();
 
