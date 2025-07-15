47729a3e71cad5783ae13c78fc1305d2f7be7657
Camille JOLLIET <cjolliet@laprovence.com>
2025-07-15 09:34:04 +0200

    unit tests

--- src/app/shared/components/logout/logout.component.spec.ts
+++ src/app/shared/components/logout/logout.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogoutComponent } from './logout.component';
+import { provideHttpClient } from '@angular/common/http';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
+import { AuthService } from '../../../services/auth/auth.service';
+
+const storeMock = { select: jasmine.createSpy('select').and.returnValue(of(null)) };
 
 describe('LogoutComponent', () => {
   let component: LogoutComponent;
   let fixture: ComponentFixture<LogoutComponent>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['logout']);
     await TestBed.configureTestingModule({
-      imports: [LogoutComponent]
+      imports: [LogoutComponent],
+      providers: [
+        provideHttpClient(),
+        { provide: Store, useValue: storeMock },
+        { provide: AuthService, useValue: authServiceSpy }
+      ]
     })
     .compileComponents();
 
@@ -19,5 +32,11 @@ describe('LogoutComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call AuthService.logout on click', () => {
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    expect(authServiceSpy.logout).toHaveBeenCalled();
   });
 });
