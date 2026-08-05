3ba985d7ecc0fc4934a43d117b6ee27e23d762a6
Paytrichore <trollpetrichore@gmail.com>
2026-08-05 10:43:42 +0200

    37 - worlApi config environment et doc (#38)

---
+++ src/environments/environment.model.ts
@@ -0,0 +1,6 @@
+export interface Environment {
+  production: boolean;
+  userApiUrl: string;
+  peblobApiUrl: string;
+  worldApiUrl: string;
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   userApiUrl: 'https://papi-user-dev-812288085862.us-central1.run.app',
   peblobApiUrl: 'https://papi-peblob-dev-812288085862.us-central1.run.app',
+  worldApiUrl: 'https://papi-world-dev-812288085862.us-central1.run.app',
 };
